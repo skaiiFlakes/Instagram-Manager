a593c61af6f3e567fec50c19f4f46100b2e8d263
Sky Angeles <spangeles24@xs.edu.ph>
2021-02-09 15:56:16 +0800

    Update instaFollowerBot.py

--- instaFollowerBot.py
+++ instaFollowerBot.py
@@ -1,4 +1,10 @@
 from instapy import InstaPy
+import os
+
+try:
+    os.system("c:\\windows\\system32\\taskkill.exe /im firefox.exe /f")
+except:
+    print("firefox already closed")
 
 session = InstaPy(username = "username", password = "password") # Enter your instagram username and password
 session.login()
@@ -6,10 +12,12 @@ session.login()
 while True:
     try:
         session.set_relationship_bounds(enabled = True, max_followers = 200)
-        session.set_do_follow(True, percentage =100)
-        session.set_do_comment(enabled=True, percentage=50) # optional
-        session.like_by_tags(['tag1', 'tag2', 'tag3'], amount = 1) # Enter key words
+        session.like_by_tags(['tag1', 'tag2', 'tag3'], amount = 100) # Enter key word
+        ssession.set_do_follow(True, percentage =50)
+        session.set_dont_like(['tag1', 'tag2', 'tag3'])
         session.set_comments(['comment1', 'comment2', 'comment3']) # Set possible comments
+        session.set_do_comment(enabled=True, percentage=50) # optional
+        
     except Exception as e:
         print(e)
         pass
