eb4151fb5584bf5fec6cae27befd203132ac4cdd
Sky Angeles <spangeles24@xs.edu.ph>
2021-02-12 11:48:26 +0800

    Update instaFollowerBot.py

--- instaFollowerBot.py
+++ instaFollowerBot.py
@@ -1,25 +1,59 @@
+# find out more uses of this module on https://instapy.org/settings/
 from instapy import InstaPy
 import os
+import time
 
-try:
-    os.system("c:\\windows\\system32\\taskkill.exe /im firefox.exe /f")
-except:
-    print("firefox already closed")
+#Enter instagram account details
+username = ""
+password = ""
 
-session = InstaPy(username = "username", password = "password") # Enter your instagram username and password
-session.login()
-
-while True:
+def start():
     try:
-        session.set_relationship_bounds(enabled = True, max_followers = 200)
-        session.like_by_tags(['tag1', 'tag2', 'tag3'], amount = 100) # Enter key word
-        ssession.set_do_follow(True, percentage =50)
-        session.set_dont_like(['tag1', 'tag2', 'tag3'])
-        session.set_comments(['comment1', 'comment2', 'comment3']) # Set possible comments
-        session.set_do_comment(enabled=True, percentage=50) # optional
-        
-    except Exception as e:
-        print(e)
-        pass
+        os.system("c:\\windows\\system32\\taskkill.exe /im firefox.exe /f") #Ensure that the path to taskkill.exe is correct
+    except:
+        print("firefox already closed")
 
-# find out more uses of this module on https://instapy.org/settings/
+def follow_by_tags():
+    session = InstaPy(username = username, password = password, headless_browser=True)
+    session.login()
+    session.set_relationship_bounds(enabled = True, max_followers = 200)
+    while True:
+        try:
+            session.set_do_follow(True, percentage = 100)
+            session.set_comments([]) #Enter a list of comments
+            session.set_do_comment(enabled=True, percentage=60)
+            session.set_dont_like([]) #Enter a list of tags you don't want to include
+            session.like_by_tags([], amount = 6) #Enter a list of tags you want to include
+        except:
+            pass
+
+def follow_fof():
+    session = InstaPy(username = username, password = password, headless_browser=True)
+    session.login()
+    while True:
+        try:
+            accounts = [] # Enter a list of chosen followers
+            session.follow_user_followers(accounts, amount=20, randomize = False)
+        except:
+            pass
+
+def unfollow():
+    session = InstaPy(username = username, password = password, headless_browser=True)
+    session.login()
+    while True:
+        try:
+            session.unfollow_users(amount="full", instapy_followed_enabled=True, instapy_followed_param="nonfollowers", style="FIFO", unfollow_after=90*60*60, sleep_delay=501)
+        except:
+            pass
+
+while userinput != 0 and userinput != 1 and userinput != 2:
+    userinput = int(input("\n0: Follow by tags\n1: Follow Followers' Followers\n2: Unfollow Nonfollowers Followed by InstaPy\n\nEnter: "))
+if userinput == 0:
+    start()
+    follow_by_tags()
+elif userinput == 1:
+    start()
+    follow_fof()
+elif userinput == 2:
+    start()
+    unfollow()
